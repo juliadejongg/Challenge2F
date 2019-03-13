94720076056f08775207e377e3b98650b956357b
juliadejongg <juliadejongg@hotmail.com>
2019-03-13 14:16:29 +0100

    Cirkels aangepast met css

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,6 +1,4 @@
 var circle = document.getElementById('circle');
 
-var tl = new TimelineMax ({repeat:-1});
-    tl.add( TweenLite.from("#circle", 5, {y:500, opacity:0, scale: 1}) );
-    tl.add( TweenLite.to("#circle", 5, {x:250, ease: Elastic.easeInOut}) );
-    tl.add( TweenLite.to("#circle", 5, {y: 500, x:250, opacity:0}) );
+var tl = new TimelineMax ();
+    tl.add( TweenLite.from("#circle", 10, {y:500, opacity:0, scale: 1}) );
