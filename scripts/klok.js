02a02556576e56e83837aae77b4e2c9384e0d4d5
juliadejongg <juliadejongg@hotmail.com>
2019-03-13 07:08:03 +0100

    Js gefixt

--- scripts/klok.js
+++ scripts/klok.js
@@ -24,65 +24,49 @@ function clock() {// We create a new Date object and assign it to a variable cal
 
 //
 function changeBackground () {
-	var hours = new Date().getHours();
+  var hours = new Date().getHours();
+  var tlm = new TimelineMax();
 
 	if (hours >= 6 && hours <= 12) {
     document.body.className = "dusk"; //dusk.jpg
-		document.getElementById("tekst").innerHTML = "Goedemorgen! Tijd om op te staan."; //text onder de klok 
+    document.getElementById("tekst").innerHTML = "Goedemorgen! Tijd om op te staan."; //text onder de klok 
+    tlm.set("#circle", {visibility: "hidden"});
+    tlm.set("#circle2", {visibility: "visible"});
+    tlm.set("#circle3", {visibility: "hidden"});
+    tlm.set("#circle4", {visibility: "hidden"});
 	}
 
 	else if (hours >= 13 && hours <= 17) { 
     document.body.className = "day"; //day.jpg
-		document.getElementById("tekst").innerHTML = "Fijne middag! Ga wat leuks doen.";
+    document.getElementById("tekst").innerHTML = "Fijne middag! Ga wat leuks doen.";
+    tlm.set("#circle", {visibility: "visible"});
+    tlm.set("#circle2", {visibility: "hidden"});
+    tlm.set("#circle3", {visibility: "hidden"});
+    tlm.set("#circle4", {visibility: "hidden"});
   }
 
   else if (hours >= 18 && hours <= 24) { 
-		document.body.className = "sunset"; //sunset.jpg
-		document.getElementById("tekst").innerHTML = "Het is al bijna tijd om naar bed te gaan..";
+    document.body.className = "sunset"; //sunset.jpg
+    document.getElementById("tekst").innerHTML = "Het is al bijna tijd om te gaan slapen..";
+    tlm.set("#circle", {visibility: "hidden"});
+    tlm.set("#circle2", {visibility: "hidden"});
+    tlm.set("#circle3", {visibility: "visible"});
+    tlm.set("#circle4", {visibility: "hidden"});
   }
 
   else if (hours >= 0 && hours <= 5) { 
 		document.body.className = "night"; //night.jpg
-		document.getElementById("tekst").innerHTML = "Tijd om te slapen.";
+    document.getElementById("tekst").innerHTML = "Tijd om te slapen.";
+    tlm.set("#circle", {visibility: "hidden"});
+    tlm.set("#circle2", {visibility: "hidden"});
+    tlm.set("#circle3", {visibility: "hidden"});
+    tlm.set("#circle4", {visibility: "visible"});
   }
 
-function changeCircle () {
-  var hours = new Date().getHours();
-  
-  if (hours >= 6 && hours <= 12) {
-    document.getElementById("circle").style.display = "inline"; 
-    document.getElementById("circle2").style.display = "none";
-    document.getElementById("circle3").style.display = "none";
-    document.getElementById("circle4").style.display = "none";
-  }
-  
-  else if (hours >= 13 && hours <= 17) { 
-    document.getElementById("circle").style.display = "none"; 
-    document.getElementById("circle2").style.display = "inline";
-    document.getElementById("circle3").style.display = "none";
-    document.getElementById("circle4").style.display = "none";
-  }
-  
-  else if (hours >= 18 && hours <= 24) { 
-    document.getElementById("circle").style.display = "none"; 
-    document.getElementById("circle2").style.display = "none";
-    document.getElementById("circle3").style.display = "inline";
-    document.getElementById("circle4").style.display = "none";
-  }
-  
-  else if (hours >= 0 && hours <= 5) { 
-    document.getElementById("circle").style.display = "none"; 
-    document.getElementById("circle2").style.display = "none";
-    document.getElementById("circle3").style.display = "none";
-    document.getElementById("circle4").style.display = "inline";
-  }
-  
+
 }
-
 // iit 
 setInterval(clock, 1000);
 
   changeBackground();
-  changeCircle();
-}
 
